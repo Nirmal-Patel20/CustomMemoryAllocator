0c7b0ad5c0dc8c7c61d002c1e9041f044aac5893
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-09-08 21:22:23 +0530

    Add CMake configuration and initial dummy test for Catch2 framework

---
+++ tests/Pool_allocator_tests.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch_all.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+// Dummy test case to verify Catch2 is working
+TEST_CASE("Pool allocator tests", "[pool_allocator]") {
+    REQUIRE(true);
+}
