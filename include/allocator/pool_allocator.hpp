c3d40ae9a33c4f8a2783ea020e9d22e4c3a83d2b
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-09-09 23:49:16 +0530

    Add Pool_Allocator class implementation

--- include/allocator/pool_allocator.hpp
+++ include/allocator/pool_allocator.hpp
@@ -1,10 +1,39 @@
 #ifndef POOL_ALLOCATOR_HPP
 #define POOL_ALLOCATOR_HPP
 
+#include "allocator/allocator_interface.hpp"
 #include <iostream>
+#include <vector>
 
 namespace allocator {
-class Pool_Allocator {};
+class Pool_Allocator : public AllocatorInterface {
+  public:
+    explicit Pool_Allocator(size_t blockSize, size_t blockCount,
+                            size_t alignment = alignof(std::max_align_t));
+    ~Pool_Allocator() override;
+
+    virtual void* allocate(size_t size, [[maybe_unused]] size_t alignment = alignof(std::max_align_t)) override;
+    virtual void deallocate(void* ptr) override;
+    virtual size_t getAllocatedSize() const override;
+    virtual void reset() override;
+
+  private:
+    struct pool {
+        std::unique_ptr<std::byte[]> memory;
+        size_t size = 0;
+        void* free_list_head = nullptr;
+        size_t allocated_count = 0;
+        size_t free_count = 0;
+    };
+
+    void allocate_new_pool();
+
+    size_t m_blockSize;
+    size_t m_blockCount;
+    size_t m_Alignment;
+    size_t m_poolSize;
+    std::vector<pool> pools;
+};
 
 } // namespace allocator
 
--- src/pool_allocator.cpp
+++ src/pool_allocator.cpp
@@ -1,5 +1,84 @@
-#include "allocators/pool_allocator.hpp"
+#include "allocator/pool_allocator.hpp"
 
-void greet() {
-    std::cout << "Hello, Custom Memory Allocator!" << std::endl;
+allocator::Pool_Allocator::Pool_Allocator(size_t m_blockSize, size_t initial_capacity,
+                                          size_t alignment)
+    : m_blockSize(m_blockSize), m_blockCount(initial_capacity), m_Alignment(alignment) {
+
+    std::cout << std::string(40,'-') << std::endl;
+    m_blockSize =
+        ((m_blockSize >= 8) ? m_blockSize : sizeof(void*)); // Ensure minimum size for free list
+    m_poolSize = m_blockSize * m_blockCount;
+
+    allocate_new_pool();
+}
+
+allocator::Pool_Allocator::~Pool_Allocator() {
+    pools.clear();
+}
+
+void* allocator::Pool_Allocator::allocate(size_t size,
+                                [[maybe_unused]]  size_t alignment) {
+    if (size > m_blockSize) {
+        throw std::bad_alloc(); // Requested size exceeds block size
+    }
+
+    for (auto& p : pools) {
+        if (p.free_list_head != nullptr) {
+            void* block = p.free_list_head;
+            p.free_list_head = *reinterpret_cast<void**>(block);
+            p.allocated_count++;
+            p.free_count--;
+
+            return block;
+        }
+    }
+    return nullptr;
+}
+
+void allocator::Pool_Allocator::deallocate(void* ptr) {
+    for (auto& pool : pools) {
+        auto start = pool.memory.get();
+        auto end = pool.memory.get() + pool.size;
+
+        if (ptr >= start && ptr <= end) {
+            *reinterpret_cast<void**>(ptr) = pool.free_list_head;
+            pool.free_list_head = ptr;
+            return;
+        }
+    }
+
+    throw std::runtime_error("Pointer does not belong to any pools inside this allocator");
+}
+
+size_t allocator::Pool_Allocator::getAllocatedSize() const {
+    size_t totalAllocatedSize = 0;
+    for(auto& pool : pools){
+        totalAllocatedSize += pool.size;
+    }
+
+    return totalAllocatedSize;
+}
+
+void allocator::Pool_Allocator::reset() {
+    pools.clear();
+    m_poolSize = m_blockSize * m_blockCount;
+    allocate_new_pool();
+}
+
+void allocator::Pool_Allocator::allocate_new_pool() {
+
+    pool new_pool;
+    new_pool.memory = std::make_unique<std::byte[]>(m_poolSize);
+    new_pool.size = m_poolSize;
+
+    // Initialize free list
+    for (size_t i = 0; i < m_blockCount; ++i) {
+        void* block = new_pool.memory.get() + i * m_blockSize;
+        void* aligned_block = getAlignment(block, m_Alignment); // Ensure alignment
+        *reinterpret_cast<void**>(aligned_block) = new_pool.free_list_head;
+        new_pool.free_list_head = aligned_block;
+        new_pool.free_count++;
+    }
+
+    pools.push_back(std::move(new_pool));
 }
