c9164fef92b32590eae3aaeeaf15a993e8ca32f0
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-09-09 20:12:31 +0530

    Refactor: pointer and alignment syntax in AllocatorInterface and AllocatorAdapter for consistency

--- include/allocator/allocator_interface.hpp
+++ include/allocator/allocator_interface.hpp
@@ -5,15 +5,15 @@ class AllocatorInterface {
   public:
     virtual ~AllocatorInterface() = default;
 
-    virtual void *allocate(size_t size, size_t alignment = alignof(std::max_align_t)) = 0;
-    virtual void deallocate(void *ptr) = 0;
+    virtual void* allocate(size_t size, size_t alignment = alignof(std::max_align_t)) = 0;
+    virtual void deallocate(void* ptr) = 0;
     virtual size_t getAllocatedSize() const = 0;
     virtual void reset() = 0;
 
-    static void *getAlignment(void *ptr, size_t alignment) {
+    static void* getAlignment(void* ptr, size_t alignment) {
         uintptr_t addr = reinterpret_cast<uintptr_t>(ptr);
         auto alignAddr = (addr + alignment - 1) & ~(alignment - 1);
-        return reinterpret_cast<void *>(alignAddr);
+        return reinterpret_cast<void*>(alignAddr);
     }
 };
 
@@ -27,15 +27,15 @@ template <typename T> class AllocatorAdapter {
   public:
     using value_type = T;
 
-    explicit AllocatorAdapter(AllocatorInterface *alloc) : allocator_(alloc) {}
+    explicit AllocatorAdapter(AllocatorInterface* alloc) : allocator_(alloc) {}
 
-    T *allocate(size_t n) {
-        return static_cast<T *>(allocator_->allocate(n * sizeof(T), alignof(T)));
+    T* allocate(size_t n) {
+        return static_cast<T*>(allocator_->allocate(n * sizeof(T), alignof(T)));
     }
 
-    void deallocate(T *ptr, size_t) { allocator_->deallocate(ptr); }
+    void deallocate(T* ptr, size_t) { allocator_->deallocate(ptr); }
 
   private:
-    AllocatorInterface *allocator_;
+    AllocatorInterface* allocator_;
 };
 } // namespace allocator
