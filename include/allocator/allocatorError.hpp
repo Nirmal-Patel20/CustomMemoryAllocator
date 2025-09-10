bd7e6c38b342805b6a652d711c7eabb64bef3c33
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-09-10 20:42:52 +0530

    Add allocatorError.hpp for configurable error handling in memory allocation which will make debugging easier

---
+++ include/allocator/allocatorError.hpp
@@ -0,0 +1,20 @@
+#ifndef ALLOCATORERROR_HPP
+#define ALLOCATORERROR_HPP
+
+#include <stdexcept>
+#include <string>
+
+// make error handling configurable. In debug mode, throw detailed exceptions.
+// In release mode, throw std::bad_alloc for allocation failures.
+namespace allocator {
+
+inline void throwAllocationError([[maybe_unused]] std::string allocationType, [[maybe_unused]] std::string message) {
+    #if defined(ALLOCATOR_DEBUG)
+        throw std::runtime_error("Allocation Error in " + allocationType + ": " + message);
+    #else
+        throw std::bad_alloc();
+    #endif
+}
+}
+
+#endif // ALLOCATORERROR_HPP
