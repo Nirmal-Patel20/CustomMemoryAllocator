5fe9fb605579d724340ec4932f152a46da32d2e4
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-09-20 01:52:29 +0530

    test: add basic stack allocator tests

---
+++ include/allocator/stack_allocator.hpp
@@ -0,0 +1,44 @@
+#include "allocator/allocator_interface.hpp"
+#include <vector>
+
+namespace allocator {
+
+class stack_allocator : public AllocatorInterface {
+  public:
+    stack_allocator(size_t bufferSize, size_t alignment = 0, bool m_resizable = false);
+    ~stack_allocator() override;
+
+    void* allocate(size_t size, size_t alignment = 0) override;
+    void deallocate(void* ptr = nullptr) override;
+    size_t getAllocatedSize() const override;
+    size_t getObjectSize() const override;
+    void reset() override;
+    void releaseMemory();
+
+  private:
+    void allocate_new_buffer();
+
+    // To track last allocation for deallocation
+    struct allocation_info {
+        void* ptr;   // Pointer to allocated memory
+        size_t size; // Size of allocation
+    };
+
+    // Pre-allocated memory buffer
+    struct buffer {
+        std::unique_ptr<std::byte[]> memory; // Contiguous memory
+        size_t size;                         // Total buffer size
+        size_t offset = 0;                   // Current allocation offset
+    };
+
+    size_t m_alignment;                              // Default alignment
+    size_t m_bufferSize;                             // Default buffer size
+    std::vector<buffer> buffers;                     // All allocated buffers
+    std::vector<allocation_info> allocation_history; // Track allocations for LIFO deallocation
+    bool m_resizable = false;                        // configurable
+    bool m_ownsMemory = false;                       // check if the allocator owns the memory
+    static constexpr size_t MAX_CAPACITY =
+        64ull * 1024 * 1024; // 64 MB Max capacity if it's not resizable
+};
+
+} // namespace allocator
