71820402628d974cabfcdfa36a3d9f11b396a853
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-09-27 17:26:51 +0530

    fix: Now ALLOCATOR_DEBUG is always defined feat: add Max_Capacity_checks so that on debug mode allow to bypass Max capacity ig: benchmark

--- include/allocator/AllocatorUtilities.hpp
+++ include/allocator/AllocatorUtilities.hpp
@@ -1,9 +1,19 @@
 #ifndef ALLOCATORUTILITIES_HPP
 #define ALLOCATORUTILITIES_HPP
 
+#include <atomic>
+#include <iostream>
 #include <stdexcept>
 #include <string>
-#include <atomic>
+
+// Guarantee ALLOCATOR_DEBUG is always defined
+#ifndef ALLOCATOR_DEBUG
+#ifdef NDEBUG
+#define ALLOCATOR_DEBUG 0
+#else
+#define ALLOCATOR_DEBUG 1
+#endif
+#endif
 
 namespace allocator {
 
@@ -11,15 +21,25 @@ namespace allocator {
 // In release mode, throw std::bad_alloc for allocation failures.
 inline void throwAllocationError([[maybe_unused]] std::string allocationType,
                                  [[maybe_unused]] std::string message) {
-    #if defined(ALLOCATOR_DEBUG)
-        throw std::runtime_error("Allocation Error in " + allocationType + ": " + message);
-    #else
-        throw std::bad_alloc();
-    #endif
+#if ALLOCATOR_DEBUG
+    throw std::runtime_error("Allocation Error in " + allocationType + ": " + message);
+#else
+    throw std::bad_alloc();
+#endif
 }
 
-// Global flag controlling debug checks
-inline std::atomic<bool> g_debug_checks{true};
+// Global flag controlling debug checks and Capacity_checks
+#if ALLOCATOR_DEBUG
+inline std::atomic<bool> g_debug_checks = true;
+#else
+inline constexpr std::atomic<bool> g_debug_checks = false;
+#endif
+
+#if ALLOCATOR_DEBUG
+inline std::atomic<bool> Max_Capacity_checks = true;
+#else
+inline constexpr std::atomic<bool> Max_Capacity_checks = true;
+#endif
 
 } // namespace allocator
 
